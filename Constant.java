460d968f71e411cf08884e0ee815c25602894031
nur127 <nuralamshikder614@gmail.com>
2024-10-04 22:58:03 +0600

    STEP #5.	Replaced, string literals with constants, storing those constants in a new class called Constants.java

---
+++ Constant.java
@@ -0,0 +1,4 @@
+public class Constant {
+    protected final String LOADING_MESSAGE = "Your Data Is Loading.... ";
+    protected  final String  WRONG_ARGUMENT_MESSAGE = "Invalid Argument . Please Give an valid Argument like : (a,r,c) ?Studentname +Studentname + Studentname";
+}
